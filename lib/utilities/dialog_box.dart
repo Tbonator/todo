d4a917e784637527514762e6780ed40e90c90c04
Tbonator <thabo2g5@yahoo.ca>
2024-03-21 12:20:28 +0200

    adding slider

--- lib/utilities/dialog_box.dart
+++ lib/utilities/dialog_box.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors_in_immutables, prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors_in_immutables, prefer_typing_uninitialized_variables, sized_box_for_whitespace, must_be_immutable
 
 import 'package:flutter/material.dart';
 import 'package:todo/utilities/my_button.dart';
